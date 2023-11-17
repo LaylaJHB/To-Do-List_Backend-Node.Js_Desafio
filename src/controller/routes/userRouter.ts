a92833b6a0f32b8faaf1c7dacc494108945d4752
Layla Janaína Hissa Borges <99913142+LaylaJHB@users.noreply.github.com>
2023-11-17 09:11:36 -0300

    opmentários de rotas

--- src/controller/routes/userRouter.ts
+++ src/controller/routes/userRouter.ts
@@ -16,3 +16,20 @@ userRouter.get("/getAll",(req, res) => userController.getUsers(req, res))
 userRouter.post('/create',(req, res) => userController.createUser(req, res))
 
 userRouter.post('/login',(req, res) => userController.login(req, res))
+
+
+// Projeto (nome do domínio): todolist.com -> retorna a página home do site - GET todolist.com 
+
+//Criar tarefa: POST todolist.com/task 
+
+//Buscar todas as tarefas: 
+    //sem filtros:
+    // GET todolist.com/task
+
+    //paginado: 
+    // GET todolist.com/task?page=2&orderby=title
+    
+
+//Deletar uma tarefa: DELETE todolist.com/task/:id
+
+//Editar uma tarefa: POST todolist.com/task/:id
